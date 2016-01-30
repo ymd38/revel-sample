2af1637d21b4f0d4033bf9441f668c79f5d76c3c
hirokazu yamada <yamadaman51@yahoo.co.jp>
2016-01-31 03:40:44 +0900

    ddl

---
+++ sql/init.ddl.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE `issue_management` DEFAULT CHARACTER SET utf8;
+
+USE `issue_management`;
+
+CREATE TABLE IF NOT EXISTS `issue` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `title` varchar(1024) NOT NULL COMMENT 'タイトル',
+  `source` varchar(1024) NOT NULL COMMENT '情報元',
+  `detail` text NOT NULL COMMENT '詳細',
+  `priority` tinyint(1) NOT NULL DEFAULT '1' COMMENT '対応状況(0:重要, 1:緊急, 2:その他)',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '対応状況(0:完了, 1:未対応)',
+  `created` datetime NOT NULL COMMENT '作成日時',
+  `updated` datetime NOT NULL COMMENT '更新日時',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='脆弱性情報';
+
+-- CREATE INDEX issue_emergency_no_complite ON issue(`title`);
