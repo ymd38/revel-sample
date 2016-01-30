38ffe6ab3e696075c5cbf7555fc04d9690448262
hirokazu yamada <yamadaman51@yahoo.co.jp>
2016-01-30 20:51:41 +0900

    不要な箇所の削除

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,13 +1,8 @@
 package controllers
 
-import (
-	"fmt"
-
-	"github.com/revel/revel"
-)
+import "github.com/revel/revel"
 
 func init() {
-	fmt.Println("init.go:init")
 	revel.OnAppStart(InitDB)
 	//revel.InterceptMethod(App.Auth, revel.BEFORE)
 }
