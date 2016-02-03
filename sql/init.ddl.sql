270cccdbd818537b0eb6d54653b0f7d0aee417c7
Hirokazu Yamada <h-yamada@mediba.jp>
2016-02-03 17:22:29 +0900

    - テーブル追加、変更 - テストデータ

--- sql/init.ddl.sql
+++ sql/init.ddl.sql
@@ -9,11 +9,12 @@ CREATE TABLE IF NOT EXISTS `issue` (
   `detail` text NOT NULL COMMENT '詳細',
   `priority` tinyint(1) NOT NULL DEFAULT '1' COMMENT '対応状況(0:重要, 1:緊急, 2:その他)',
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '対応状況(0:完了, 1:未完了)',
-  `limit` char(8)  NOT NULL COMMENT '期限日',
-  `created` int NOT NULL COMMENT '作成日時',
-  `updated` int NOT NULL COMMENT '更新日時',
+  `limit` int NOT NULL COMMENT '期限日(UNIX_TIMESTAMP)',
+  `created` int NOT NULL COMMENT '作成日時(UNIX_TIMESTAMP)',
+  `updated` int NOT NULL COMMENT '更新日時(UNIX_TIMESTAMP)',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='脆弱性情報';
+
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -21,6 +22,43 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(1024) NOT NULL COMMENT 'パスワード',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='ユーザー';
+
+CREATE TABLE IF NOT EXISTS `service` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `name` varchar(1024) NOT NULL COMMENT 'サービス名',
+  `start` int DEFAULT null COMMENT 'サービス開始日(UNIX_TIMESTAMP)',
+  `end` int DEFAULT null COMMENT 'サービス終了日(UNIX_TIMESTAMP)',
+  `created` int NOT NULL COMMENT '作成日時(UNIX_TIMESTAMP)',
+  `updated` int NOT NULL COMMENT '更新日時(UNIX_TIMESTAMP)',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='サービス';
+
+CREATE TABLE IF NOT EXISTS `service_member` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `service_id` int(11) unsigned NOT NULL COMMENT 'サービスID',
+  `user_id` int(11) unsigned NOT NULL COMMENT 'ユーザID',
+  `created` int NOT NULL COMMENT '作成日時(UNIX_TIMESTAMP)',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='サービス担当';
+
+CREATE TABLE IF NOT EXISTS `service_issue` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `service_id` int(11) unsigned NOT NULL COMMENT 'サービスID',
+  `issue_id` int(11) unsigned NOT NULL COMMENT 'ユーザID',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '対応状況(0:完了, 1:未確認, 2:対応不要、3:確認中、4:対応予定)',
+  `limit` int DEFAULT 0 COMMENT '対応予定日(UNIX_TIMESTAMP)',
+  `memo` text DEFAULT NULL COMMENT 'メモ',
+  `created` int NOT NULL COMMENT '作成日時(UNIX_TIMESTAMP)',
+  `updated` int NOT NULL COMMENT '更新日時(UNIX_TIMESTAMP)',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='サービス対応状況';
+
+--テストデータ
+insert into issue values(1,'apache脆弱性', '社内', 'バージョンアップが必要', 1, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+insert into user values(1, 'ymd38', 'aa');
+insert into service values(1, 'サイトA', UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), UNIX_TIMESTAMP(), UNIX_TIMESTAMP());
+insert into service_member values(NULL, 1, 1,  UNIX_TIMESTAMP());
+insert into service_issue values(NULL, 1,1,1,0,NULL,UNIX_TIMESTAMP(),UNIX_TIMESTAMP());
 
 --GRANT SELECT, INSERT, UPDATE, DELETE ON issue_management.* TO 'issue_manager'@'localhost' IDENTIFIED BY 'Example_passwd123_';
 
