1ec06ad784e5ab4301eff745bca58adfe273edb6
Hirokazu Yamada <h-yamada@mediba.jp>
2016-03-10 17:50:46 +0900

    修正中

--- app/models/user_data.go
+++ app/models/user_data.go
@@ -1,28 +1,56 @@
 package models
 
-import "github.com/revel/revel"
+import (
+	"errors"
+	"regexp"
 
+	"github.com/revel/revel"
+)
+
+// UserData is struct of user data management
+// that uses database and api request/response
 type UserData struct {
-	Id       int    `json:"id"`
+	ID       int    `json:"id"`
 	Name     string `json:"name"`
+	MailAddr string `json:"mailaddr"`
 	Password string `json:"password"`
-
-	//LimitDate   time.Time
-	//CreatedDate time.Time
-	//UpdatedDate time.Time
+	Created  int64  `json:"-"`
+	Updated  int64  `json:"-"`
 }
 
-func (user *UserData) Validate(v *revel.Validation) {
-	v.Required(user.Name)
-	v.Required(user.Password)
-	/*
-		v.Match(booking.CardNumber, regexp.MustCompile(`\d{16}`)).
-			Message("Credit card number must be numeric and 16 digits")
+// Validate for userData of struct
+func (user *UserData) Validate() error {
+	var v revel.Validation
+	v.Match(user.Name, regexp.MustCompile(`\w{1,30}`))
+	if v.HasErrors() {
+		return errors.New("name is validate error")
+	}
 
-		v.Check(booking.NameOnCard,
+	/*
+		v.Check(
+			issuedata.Detail,
 			revel.Required{},
-			revel.MinSize{3},
-			revel.MaxSize{70},
+			revel.MaxSize{5120},
+			revel.MinSize{1},
 		)
+		if v.HasErrors() {
+			return errors.New("detail is validate error")
+		}
+
+		v.Match(strconv.Itoa(issuedata.Priority), regexp.MustCompile(`\d{1}`))
+		if v.HasErrors() {
+			return errors.New("priority is validate error")
+		}
+
+		v.Match(strconv.Itoa(issuedata.Status), regexp.MustCompile(`\d{1}`))
+		if v.HasErrors() {
+			return errors.New("status is validate error")
+		}
+		v.Match(issuedata.LimitStr, regexp.MustCompile(`\d{8}`))
+		if v.HasErrors() {
+			return errors.New("limit is validate error")
+		}
+			)
 	*/
+	return nil
 }
