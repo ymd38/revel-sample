b82be31b75ddae20392dbc3d107e298607ee0fc3
Hirokazu Yamada <h-yamada@mediba.jp>
2016-03-15 16:28:24 +0900

    テスト実行処理の最適化

--- tests/modelserviceissuetest.go
+++ tests/modelserviceissuetest.go
@@ -18,19 +18,21 @@ func (t *ModelServiceIssueTest) Before() {
 }
 
 func (t *ModelServiceIssueTest) TestCreate() {
-	issueid, serviceid := 0, 0
-	if err := t.serviceissue.Create(issueid, serviceid); err == nil {
+	type InputIDs struct {
+		issueid, serviceid int
+	}
+
+	okInputIDs := InputIDs{1, 1}
+	ngInputIDs := []InputIDs{{0, 0}, {0, 1}, {1, 0}}
+
+	if err := t.serviceissue.Create(okInputIDs.issueid, okInputIDs.serviceid); err != nil {
 		t.Assert(false)
 	}
 
-	issueid = 1
-	if err := t.serviceissue.Create(issueid, serviceid); err == nil {
-		t.Assert(false)
-	}
-
-	serviceid = 1
-	if err := t.serviceissue.Create(issueid, serviceid); err != nil {
-		t.Assert(false)
+	for i := 0; i < len(ngInputIDs); i++ {
+		if err := t.serviceissue.Create(ngInputIDs[i].issueid, ngInputIDs[i].serviceid); err == nil {
+			t.Assert(false)
+		}
 	}
 }
 
